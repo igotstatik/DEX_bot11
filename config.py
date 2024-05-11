fb2ef8bd3c968132b1e1bc8b5a1d44849ab2695f
igotstatik <156587020+igotstatik@users.noreply.github.com>
2024-05-11 15:53:12 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+from os import environ as env
+
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = '7073249672:AAFL76t5QO8Tl4DQDpvTS16ESIK4F3u_9Ls'
+#MANIFEST_URL = 'https://raw.githubusercontent.com/XaBbl4/pytonconnect/main/pytonconnect-manifest.json'
+MANIFEST_URL = '/Users/testuser/Downloads/pytonconnect-manifest.json'
